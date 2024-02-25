ae7065502e5229c4c5b5acece009313a84f8705e
Logan Roberts <logancroberts@outlook.com>
2024-02-25 09:37:31 -0500

    Move vm conf to using /etc/nixos/hardware-configuration.nix

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -75,12 +75,9 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -107,6 +104,9 @@
       kitty
       tmux
       eza
+      fish
+      autojump
+      direnv
       fzf
       lua
       gcc
@@ -114,7 +114,6 @@
       go
       nodePackages.npm
       p7zip
-      fish
     ];
   };
 
