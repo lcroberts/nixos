3091efb38d36f68c68a28f600322a92682170051
Logan Roberts <logancroberts@outlook.com>
2024-02-26 09:12:56 -0500

    Add tmux to cli.nix

--- home/cli.nix
+++ home/cli.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ btop git direnv starship fish ];
+  home.packages = with pkgs; [ btop git direnv starship fish tmux ];
 
   programs = {
     btop = {
@@ -73,6 +73,79 @@
           style = "magenta";
         };
       };
+    };
+    tmux = {
+      enable = true;
+      clock24 = true;
+      extraConfig = ''
+        unbind C-b
+        set -g prefix C-space
+        bind C-space send-prefix
+
+        # Fix terminal colors
+        set-option -sa terminal-overrides ",xterm*:Tc"
+        # set -sg terminal-overrides ",*:RGB"
+
+        # Enable Mouse toggle
+        set -g mouse on
+        bind-key m set-option mouse \; display-message "mouse #{?mouse,on,off}"
+
+        # Shift Alt vim keys to switch windows
+        bind -n M-H previous-window
+        bind -n M-L next-window
+
+        bind-key -rn C-f run-shell "tmux neww tmux-sessionizer"
+        bind-key -r i run-shell "tmux neww tmux-cht"
+        bind-key -r h run-shell "tmux-sessionizer main"
+
+        bind-key -rn M-Up resize-pane -U 3
+        bind-key -rn M-Down resize-pane -D 3
+        bind-key -rn M-Left resize-pane -L 3
+        bind-key -rn M-Right resize-pane -R 3
+
+        # Start numbering from 1
+        set -g base-index 1
+        set -g pane-base-index 1
+        set-window-option -g pane-base-index 1
+        set-option -g renumber-windows on
+
+        # Set vi-mode
+        set-window-option -g mode-keys vi
+        # Keybindings
+        bind-key -T copy-mode-vi v send-keys -X begin-selection
+        bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+        bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+        # Enable Clear
+        bind C-space send-keys 'C-l'
+
+        # Change list-keys bind
+        bind-key -r ? list-keys -aN
+
+        # Open panes in current window
+        bind '"' split-window -v -c "#{pane_current_path}"
+        bind % split-window -h -c "#{pane_current_path}"
+
+        # Undercurl
+        # set -g default-terminal "''${TERM}"
+        set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
+        set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colours - needs tmux-3.0
+
+        # Set clipboard for tmux yank
+        set -g set-clipboard on
+        set -g @continuum-restore 'on'
+        set -g @plugin 'noscript/tmux-mighty-scroll'
+        set -g @mighty-scroll-interval 5
+      '';
+      plugins = with pkgs; [
+        tmuxPlugins.sensible
+        tmuxPlugins.vim-tmux-navigator
+        tmuxPlugins.yank
+        tmuxPlugins.open
+        tmuxPlugins.continuum
+        tmuxPlugins.resurrect
+        tmuxPlugins.catppuccin
+      ];
     };
   };
 
