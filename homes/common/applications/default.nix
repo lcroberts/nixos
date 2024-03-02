00ca099d4e892014142341ee7140b76e67141e4d
Logan Roberts <logancroberts@outlook.com>
2024-03-02 12:58:34 -0500

    Add some more default packages

--- homes/common/applications/default.nix
+++ homes/common/applications/default.nix
@@ -1,3 +1,11 @@
 {pkgs, ...}: {
   imports = [./kitty];
+  home.packages = with pkgs; [
+    onedrive
+    onedrivegui
+    obsidian
+    anki-bin
+    spotifywm
+    vesktop # discord with vencord
+  ];
 }
