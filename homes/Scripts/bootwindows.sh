f02f8e6618546887ec2177bf40525721a53260cf
Logan Roberts <logancroberts@outlook.com>
2024-03-06 12:28:55 -0500

    Remvoe unneeded files

--- homes/Scripts/bootwindows.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-sudo grub-reboot "$(sudo grep -i windows /boot/grub/grub.cfg|cut -d"'" -f2)" && sudo reboot
--- homes/Scripts/setup-monitor.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-pkill kanshi
-kanshi -c $HOME/.config/kanshi/games 2>&1 &
